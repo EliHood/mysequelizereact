73404fa9124a6522694355b5d61daf2fa5c329a8
Eli <eli@Elis-MacBook-Pro.local>
2019-04-10 17:25:47 -0400

    update

--- models/index.js
+++ models/index.js
@@ -5,32 +5,23 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
+const { Client } = require('pg');
 const config = require(__dirname + '/../config/database.js')[env];
 // console.log(config, env);
 const db = {};
 
 let sequelize;
-
-if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL,
-    {
-      dialect: "postgres",
-      protocol: "postgres",
-      port: 5432,
-      host: process.env.HOST,
-      logging: true //false
-   });
-} else if(config.use_env_variable){
+if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else{
+} else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
 
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
+client.connect();
 
 fs
   .readdirSync(__dirname)
