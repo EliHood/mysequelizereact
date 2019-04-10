a5d0575063d396c6ca418e41b1461e526b39dd9c
Eli <eli@Elis-MacBook-Pro.local>
2019-04-09 21:30:27 -0400

    update

--- config/database.js
+++ config/database.js
@@ -18,6 +18,15 @@ if (!process.env.PG_DB) {
       host: process.env.POSTGRES_HOST,
       dialect: 'postgres',
       migrationStorageTableName: 'sequelize_meta'
+    },
+
+    production: {
+      username: "root",
+      password: null,
+      database: "postgres://zhxyfgsfxgklyn:7a47a3a5ec8fe28bd7fd2426a2c47258e4d3ca029fe4a0e478a29064891dae83@ec2-23-21-234-53.compute-1.amazonaws.com:5432/dca54vcb49ocfj",
+      host: "127.0.0.1",
+      dialect: "postgres"
     }
+  
   }
   
