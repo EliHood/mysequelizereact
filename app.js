5196993574aee6a37c3cb10baf4835cdec1f4e49
Eli <eli@Elis-MacBook-Pro.local>
2019-04-09 22:54:07 -0400

    update

--- app.js
+++ app.js
@@ -14,17 +14,10 @@ const port = process.env.PORT || 8000;
 const passport = require('passport');
 const path = require('path');
 // const allowOrigin = process.env.ALLOW_ORIGIN || '*'
-
 // CORS Middleware
-
-  
-
-
-
 if (!process.env.PORT) {
   require('dotenv').config()
 }
-
 if (!process.env.PORT) {
   console.log('[api][port] 8000 set as default')
   console.log('[api][header] Access-Control-Allow-Origin: * set as default')
@@ -33,34 +26,21 @@ if (!process.env.PORT) {
   console.log(`[api][port] ${process.env.PORT}`)
   console.log(`[api][header] Access-Control-Allow-Origin: ${process.env.ALLOW_ORIGIN}`)
 }
-
-
-
-
-
-
 require('./config/passport-github');
 require('./config/passport');
-
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
-
 app.use(session({
   secret : process.env.JWT_SECRET,
   saveUninitialized: false,
   maxAge: 1000 * 60 * 60 * 84,
   resave: false
-
 }));
-
 app.use(passport.initialize());
 app.use(passport.session());
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:false})); 
-
 const isAuthenticated = function(req, res, next){
   if(req.isAuthenticated()){
     next();
@@ -77,7 +57,6 @@ const isAuthenticated = function(req, res, next){
 //   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 //   next();
 // });
-
 app.use(cors({
     'allowedHeaders': ['Content-Type'], // headers that React is sending to the API
     'exposedHeaders': ['Content-Type'], // headers that you are sending back to React
@@ -85,27 +64,16 @@ app.use(cors({
     'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
     'preflightContinue': false
 }));
-
 app.use('/api/users', userRoute );
 app.use('/api/posts', isAuthenticated,  postRoute );
 app.use(function(req, res, next) {
   res.locals.user = req.user; // This is the important line
   // req.session.user = user
-  
   console.log(res.locals.user);
   next();
 });
-
-
-
-
 models.sequelize.sync().then(() => {
   const server = app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`);
   });
 });
-
-
-
-
-
