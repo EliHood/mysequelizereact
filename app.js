172a59448bdd1870619009bca612fae21566b23f
Eli <eli@Elis-MacBook-Pro.local>
2019-04-11 10:17:01 -0400

    update

--- app.js
+++ app.js
@@ -72,10 +72,10 @@ app.use('/api/users', userRoute );
 app.use('/api/posts', isAuthenticated,  postRoute );
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.resolve(__dirname, './client/build')));
+  app.use(express.static(path.resolve(__dirname, 'client/build')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
