68206ead4a914f92162f0fc3486f5f2e0b070926
Eli <eli@Elis-MacBook-Pro.local>
2019-04-11 10:37:25 -0400

    update

---
+++ client/build/precache-manifest.71f33d9f1735edadfca05177f5133076.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb41a058698e84705f01",
+    "url": "/static/css/main.efc6a54f.chunk.css"
+  },
+  {
+    "revision": "cb41a058698e84705f01",
+    "url": "/static/js/main.cb41a058.chunk.js"
+  },
+  {
+    "revision": "20178ede868c08b8c9c8",
+    "url": "/static/js/1.20178ede.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4e57aafb1ce07f674e5767e53f86a321",
+    "url": "/index.html"
+  }
+];
