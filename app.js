0d189b59f5b55d101c0bca91d8ff2bcf623cff77
Eli <eli@Elis-MacBook-Pro.local>
2019-04-30 13:52:27 -0400

    update

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ const path = require('path');
 if (!process.env.PORT) {
   require('dotenv').config()
 }
-
 // console.log(process.env.DATABASE_URL);
 if (!process.env.PORT) {
   console.log('[api][port] 8000 set as default')
@@ -29,7 +28,6 @@ if (!process.env.PORT) {
   console.log(`[api][port] ${process.env.PORT}`)
   console.log(`[api][header] Access-Control-Allow-Origin: ${process.env.ALLOW_ORIGIN}`)
 }
-
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'build')));
@@ -37,41 +35,37 @@ app.use(cookieParser());
 app.use(session({
   secret : process.env.JWT_SECRET,
 }));
-
 require('./config/passport.js')(passport); // PASSPORT Init
 app.use(passport.initialize());
 app.use(passport.session());
-
-
 app.use(bodyParser.urlencoded({ extended:false})); 
 app.use(bodyParser.json());
-// this code may be useless or useful, still trying to understand cors. 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials',  true);
-  res.header("preflightContinue", false)
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.locals.user = req.user; // This is the important line
+  // req.session.user = user
+  console.log(res.locals.user);
   next();
 });
-
-
+// this code may be useless or useful, still trying to understand cors. 
+app.use((req, res, next) => {
+  const { headers } = req;
+  res.header('Access-Control-Allow-Origin', headers.origin);
+  res.header('Access-Control-Allow-Headers', headers);
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.use(cors({
   origin: process.env.ALLOW_ORIGIN,
   credentials: true,
   allowedHeaders: 'X-Requested-With, Content-Type, Authorization',
   methods: 'GET, POST, PATCH, PUT, POST, DELETE, OPTIONS'
 }))
-
 app.use('/api/users', userRoute );
 app.use('/api/posts', postRoute );
-
 // In order to use REACT + EXPRESS we need the following code, alone with a build
 // in the client folder we run a npm run build in the client folder then it is referred
 // in the following code. 
-
 app.use(express.static(path.join(__dirname, 'client/build')));
-
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
   //
@@ -83,22 +77,8 @@ if(process.env.NODE_ENV === 'production') {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/public/index.html'));
 })
-
-
-
-app.use(function(req, res, next) {
-  res.locals.user = req.user; // This is the important line
-  // req.session.user = user
-  console.log(res.locals.user);
-  next();
-});
-
-
-
 models.sequelize.sync().then(function() {
-
   app.listen(PORT, host, () => {
     console.log('[api][listen] http://localhost:' + PORT)
   })
-
 })
