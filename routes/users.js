8c74fd8ce454498802708b9405e946580ac6dd3a
Eli <eli@Elis-MacBook-Pro.local>
2019-06-09 03:27:43 -0400

    remove dup code

--- routes/users.js
+++ routes/users.js
@@ -187,43 +187,7 @@ router.get('/reset', (req, res, next) => {
     }
   });
 });
-router.put('/updatePassword', (req, res, next) => {
-  passport.authenticate('jwt', { session: false }, (err, user, info) => {
-    if (err) {
-      console.log(err);
-    }
-    if (info != undefined) {
-      console.log(info.message);
-      res.status(403).send(info.message);
-    } else {
-      models.User.findOne({
-        where: {
-          username: req.body.username,
-        },
-      }).then(user => {
-        if (user != null) {
-          console.log('user found in db');
-          bcrypt
-            .hash(req.body.password, BCRYPT_SALT_ROUNDS)
-            .then(hashedPassword => {
-              user.update({
-                password: hashedPassword,
-              });
-            })
-            .then(() => {
-              console.log('password updated');
-              res
-                .status(200)
-                .send({ auth: true, message: 'password updated' });
-            });
-        } else {
-          console.log('no user exists in db to update');
-          res.status(404).json('no user exists in db to update');
-        }
-      });
-    }
-  })(req, res, next);
-});
+
 router.put('/updatePassword', (req, res, next) => {
   passport.authenticate('jwt', { session: false }, (err, user, info) => {
     if (err) {
